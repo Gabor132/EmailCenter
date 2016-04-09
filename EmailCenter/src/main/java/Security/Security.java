5ac4de4c769bf9d95167ef071f00a4f2c444be81
Dragos-Alexandru Gabor <gabordragos@gmail.com>
2016-04-09 16:29:18 +0300

    Enhanced Security and added DatabaseHandler.java

--- EmailCenter/src/main/java/Security/Security.java
+++ EmailCenter/src/main/java/Security/Security.java
@@ -9,13 +9,14 @@ import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.KeyGeneratorSpi;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import javax.swing.JOptionPane;
 import javax.xml.bind.DatatypeConverter;
 
@@ -51,9 +52,22 @@ public abstract class Security {
         return output;
     }
     
-    public static String encrypt(String input, SecretKey key){
+    /**
+     * Encrypts input with the keyString given
+     * @param input
+     * @param keyString
+     * @return
+     */
+    public static String encrypt(String input, String keyString){
         
-        String output = "";
+        //Makes sure the keyString has 16 bytes
+        if(keyString.length() > 16){
+            keyString = keyString.substring(0, 16);
+        }
+        //Generates key from user password
+        SecretKeySpec key = new SecretKeySpec(keyString.getBytes(), "Blowfish");
+        
+        String output;
         
         try {
             Cipher cipher = Cipher.getInstance("Blowfish");
@@ -61,34 +75,49 @@ public abstract class Security {
             
             byte[] encrypted = cipher.doFinal(input.getBytes("UTF8"));
             output = DatatypeConverter.printBase64Binary(encrypted);
-            
+            return output;
         } catch (NoSuchAlgorithmException | NoSuchPaddingException 
                 | InvalidKeyException | IllegalBlockSizeException 
                 | BadPaddingException | UnsupportedEncodingException ex) {
-            JOptionPane.showMessageDialog(null, "Failed to encrypt user account password", "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Failed to encrypt user account password " + keyString + "\n" +
+                    ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             System.exit(-1);
         }
-        
-        return output;
+        return null;
     }
     
-    public static String decrypt(String input, SecretKey key){
-        String output = "";
+    /**
+     * Decrypts input with the keyString given
+     * @param input
+     * @param keyString
+     * @return
+     */
+    public static String decrypt(String input, String keyString){
+        
+        //Makes sure the keyString has 16 bytes
+        if(keyString.length() > 16){
+            keyString = keyString.substring(0, 16);
+        }
+        
+        //Generate key from user password
+        SecretKeySpec key = new SecretKeySpec(keyString.getBytes(), "Blowfish");
+        
+        String output;
         try {
             Cipher cipher = Cipher.getInstance("Blowfish");
             cipher.init(Cipher.DECRYPT_MODE, key);
             byte[] auxBytes = DatatypeConverter.parseBase64Binary(input);
             byte[] decrypted = cipher.doFinal(auxBytes);
             output = new String(decrypted,"UTF8");
-            
+            return output;
         } catch (NoSuchAlgorithmException | NoSuchPaddingException 
                 | InvalidKeyException | IllegalBlockSizeException 
                 | BadPaddingException | UnsupportedEncodingException ex) {
-            JOptionPane.showMessageDialog(null, "Failed to decrypt user account password", "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Failed to decrypt user account password "+ keyString + "\n" +
+                    ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
             System.exit(-1);
         }
-        
-        return output;
+        return null;
     }
     
 }
--- EmailCenter/src/main/java/com/emailcenter/emailcenter/Interface/MainFrame.java
+++ EmailCenter/src/main/java/com/emailcenter/emailcenter/Interface/MainFrame.java
@@ -96,24 +96,18 @@ public class MainFrame extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        
+        //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                MainFrame frame = new MainFrame();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
         });
     }
 
